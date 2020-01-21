a1bb60260b1d9a4a390e81f92da990e8d6883ac7
Michał Śnieżko <michal.sniezko@softwaremind.com>
2020-01-21 13:47:03 +0100

    first test passed

---
+++ src/IAmAliveController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class IAmAliveController
+{
+    public function handle(Request $request): Response
+    {
+        return new Response('', 200);
+    }
+}

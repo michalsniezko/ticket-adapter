ea23ff6a3d81dca6b45667ee77e5f393690e4d8f
Michał Śnieżko <michal.sniezko@softwaremind.com>
2020-01-23 06:52:07 +0100

    added AddTicketToDataStoreHandlerTest

---
+++ tests/Application/AddTicketToDataStoreHandlerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Tests\Application;
+
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class AddTicketToDataStoreHandlerTest extends WebTestCase
+{
+    /** @test */
+    public function itHandlesATicket()
+    {
+        // get text from request.json, process it through the code and expect response back that is in response.json
+        $request = file_get_contents(__DIR__ . 'AddCommentToDataStoreHandlerTest/request.json');
+
+
+    }
+}
